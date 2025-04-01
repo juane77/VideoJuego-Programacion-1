fc8869b0f1737ecddcad7d5d4dece275aa97fc9d
juane77 <juaneloyortizlara@gmail.com>
2025-04-01 10:49:59 +0200

    Actualizacion App.java

--- App.java
+++ App.java
@@ -3,20 +3,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/*
+/**
  * Este programa verifica si existe un archivo de configuracion, si existe se sale del programa, si no existe 
  * te lo crea ademas tambien te crea tres directorios.
  */
 public class App {
     public static void main(String[] args) {
-        Path rutaArchivoConfiguracion = Paths.get("configuracion/configuracion.txt");
+        Path rutaArchivoConfiguracion = Paths.get("configuracion.txt");
         if (Files.exists(rutaArchivoConfiguracion)) {
             System.out.println("El archivo de configuracion ya existe");
         } else {
             try {
-                Path rutaDirectorioEscenarios = Paths.get("configuracion/escenarios");
-                Path rutaDirectorioJugadores = Paths.get("configuracion/jugadores");
-                Path rutaDirectorioPartidas = Paths.get("configuracion/partidas");
+                Path rutaDirectorioEscenarios = Paths.get("escenarios");
+                Path rutaDirectorioJugadores = Paths.get("jugadores");
+                Path rutaDirectorioPartidas = Paths.get("partidas");
 
                 Files.createDirectories(rutaDirectorioEscenarios);
                 Files.createDirectories(rutaDirectorioJugadores);
